c4797c29e05adecf645d62ed7c43e4151755fa5b
TomBearpark <t.bearpark@googlemail.com>
2022-12-19 17:28:18 +0000

    Update plot_poly.R

--- R/plot_poly.R
+++ R/plot_poly.R
@@ -9,7 +9,7 @@
 plot_rf_poly <- function(tab){
   ggplot2::ggplot(tab)  +
     ggplot2::geom_hline(yintercept = 0, color = "grey", alpha = 0.9) +
-    ggplot2::geom_line(aes(x = temp ,y = response)) +
-    ggplot2::geom_ribbon(aes(x = temp, ymax = upper, ymin = lower), alpha=0.5,
-                fill = "blue")
+    ggplot2::geom_line(ggplot2::aes(x = temp ,y = response)) +
+    ggplot2::geom_ribbon(ggplot2::aes(x = temp, ymax = upper, ymin = lower), 
+                         alpha=0.5, fill = "blue")
 }
