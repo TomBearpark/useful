ed9dfc50eebca3d788370ace76b433ece8bc3dee
TomBearpark <t.bearpark@googlemail.com>
2022-12-19 17:25:14 +0000

    docs for plot poly

--- R/plot_poly.R
+++ R/plot_poly.R
@@ -1,4 +1,11 @@
 
+#' Plot the output of a predict_poly call
+#'
+#' @param tab : the output of predict_poly()
+#'
+#' @return a ggplot object with some nice color bars
+#' @export
+#'
 plot_rf_poly <- function(tab){
   ggplot2::ggplot(tab)  +
     ggplot2::geom_hline(yintercept = 0, color = "grey", alpha = 0.9) +
