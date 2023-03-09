8fae2b3d2e67b1154a7beb28a76d9c1d06e28e7d
TomBearpark <t.bearpark@googlemail.com>
2023-03-09 16:01:25 -0500

    cumulative lags

--- R/predict_poly_sum_lags.R
+++ R/predict_poly_sum_lags.R
@@ -4,11 +4,8 @@
 #' @param var The string identifier for thevariable we want to get a RF for.
 #' For this to work, you need
 #' to make sure that the polynomial terms are consistently named, and include
-#' this string. For example, if you wnat to predict for tavg, you could run a
-#' regression on tavg_p1 + tavg_p2. The function automatically selects these
-#' variables from the fixest ouutput. If you want to mannualy specify the
-#' coefficients to use in the prediciton, set this to NULL, and impose them in
-#' the `coefs` argument
+#' this string. They need to be named "l{lag}_{var}_p{poly}" at the moment
+#' 
 #'
 #' @param min The minimum value we want to predic the RF for
 #' @param max THe max value we want to predict the RF for
@@ -30,49 +27,66 @@
 #' predict_poly(m, "tavg", 10, 30, 20)
 #'
 
-predict_poly <- function(m, var, min, max, ref, ci_level = 95,
-                         step.length = 1, coefs = NULL,
-                         id.col = NULL, xvar_name = "temp"){
-
+predict_poly_sum_lags <- function(m, 
+                                  var, 
+                                  min, max, ref, 
+                                  ci_level = 95,
+                                  step.length = 1, coefs = NULL,
+                                  id.col = NULL, xvar_name = "temp"){
+  
+  
+  
   # Extract the coefs as a matrix
   if(is.null(coefs)){
     coefs <- stringr::str_subset(names(stats::coef(m)), var)
   }
-
+  
   beta  <- as.matrix(stats::coef(m)[coefs])
-
+  
   # Store the number of coefficients (the polynomial order)
   K     <- length(coefs)
+  
   # Store the number of (temperature) values we are going to output
   Nt    <- (max - min )/step.length + 1
-
-  # Get auxilliary matrices for predictions
+  
   TT <- matrix(nrow = Nt, ncol = K)
-  for(kk in 1:K) TT[,kk] <- seq(min, max, step.length)^kk - ref^kk
-
+  colnames(TT) <- coefs
+  
+  # Get auxilliary matrices for predictions
+  for(kk in coefs) {
+    var <- str_split(kk, "_", simplify = TRUE)
+    
+    # We need to check for the right naming scheme here, prevent bugs
+    stopifnot(dim(var)[1] == 1 & dim(var)[2] == 3)
+    stopifnot(str_length(var[1]) == 2 & str_detect(var[1], "l"))
+    stopifnot(str_length(var[3]) == 2 & str_detect(var[3], "p"))
+    
+    pp <- as.numeric(str_extract(var[3], "[[:digit:]]+"))
+    TT[,kk] <- seq(min, max, step.length)^pp - ref^pp
+  }
+  
+  beta <- as.matrix(beta[colnames(TT),] )
   # Get the predicted values
-  xb <- TT %*% beta
-
-  # Get the SE by the delta method
+  xb  <- TT %*% beta
   ## Extract relevant portion of the covariance matrix
-  sig <- stats::vcov(m)[coefs, coefs]
-
+  sig <- stats::vcov(m)[colnames(TT), colnames(TT)]
+  
   # Calculate SE at each t value using delta method
   se <- purrr::map_dbl(1:Nt,
-                function(tt){
-                  Amat <- t(matrix(TT[tt,]))
-                  sqrt(Amat %*% sig %*% t(Amat))
-                })
-
+                       function(tt){
+                         Amat <- t(matrix(TT[tt,]))
+                         sqrt(Amat %*% sig %*% t(Amat))
+                       })
+  
   # Calcutate normal approximation to critical values for rescaling SEs
   cv <- stats::qnorm((100-(100-ci_level)/2)/100)
-
+  
   # Coefficient output formatting
   output <- dplyr::tibble(!!xvar_name := seq(min, max, step.length),
                           response = drop(xb), se = se)
   output$upper <- output$response + cv*se
   output$lower <- output$response - cv*se
-
+  
   if(!is.null(id.col)) output$id <- id.col
   return(output)
 }
