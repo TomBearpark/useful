d13cdfff8024216d4f19a92d4c112e4fb4a465c3
TomBearpark <t.bearpark@googlemail.com>
2023-10-03 16:40:27 -0400

    easier plotting

--- R/plot_poly.R
+++ R/plot_poly.R
@@ -6,10 +6,35 @@
 #' @return a ggplot object with some nice color bars
 #' @export
 #'
-plot_rf_poly <- function(tab, xvar = "temp"){
-  ggplot2::ggplot(tab)  +
-    ggplot2::geom_hline(yintercept = 0, color = "grey", alpha = 0.9) +
-    ggplot2::geom_line(ggplot2::aes(x = .data[[xvar]] ,y = response)) +
-    ggplot2::geom_ribbon(ggplot2::aes(x = .data[[xvar]], ymax = upper, ymin = lower), 
-                         alpha=0.5, fill = "blue")
+plot_rf_poly <- function(tab, xvar = "temp",
+                         fill.var = 'blue',
+                         facet.var = NULL,
+                         scales = NULL){
+
+  # Set up plot
+  pp <- ggplot2::ggplot(tab, aes(x = .data[[xvar]]))  +
+    ggplot2::geom_hline(yintercept = 0, color = "grey", alpha = 0.9)
+
+
+  if(fill.var == 'blue'){
+    pp <- pp +
+      ggplot2::geom_line(ggplot2::aes(y = response)) +
+      ggplot2::geom_ribbon(ggplot2::aes(x = .data[[xvar]],
+                                        ymax = upper, ymin = lower),
+                           alpha=0.5, fill = fill)
+  }else{
+    pp <- pp +
+      ggplot2::geom_line(ggplot2::aes(y = response,
+                                      color = .data[[fill.var]])) +
+      ggplot2::geom_ribbon(ggplot2::aes(x = .data[[xvar]],
+                                        ymax = upper, ymin = lower,
+                                        fill = .data[[fill.var]]),
+                           alpha=0.5)
+  }
+
+  if(!is.null(facet.var)) pp <- pp +
+      ggplot2::facet_wrap(as.formula(paste("~", facet.var)),
+                          scales = scales)
+
+  pp
 }
